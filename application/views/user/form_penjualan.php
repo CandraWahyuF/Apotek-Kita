5b6173309431dc828d84fef7b6ec3e2ce7213544
dellasalsabila <83354350+CandraWahyuF@users.noreply.github.com>
2022-04-20 20:57:56 +0700

    title warna

--- application/views/user/form_penjualan.php
+++ application/views/user/form_penjualan.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary"><?= $title; ?></h6>
+            <h6 class="m-0 font-weight-bold text-dark"><?= $title; ?></h6>
         </div>
 
         <div class="row">
