c09c16f38a553533fde37f2bdb2e439c5a461d93
AhmadAldiSetiawan <90861380+AhmadAldiSetiawan@users.noreply.github.com>
2022-04-21 00:35:51 +0700

    Menambahkan Fitur Export File pada Lihat Kategori

--- application/views/user/lihat_kategori.php
+++ application/views/user/lihat_kategori.php
@@ -21,6 +21,17 @@
                 <!-- button tambah kategori -->
                 <a href="<?php echo base_url('user/form_kategori'); ?>"><button class="btn btn-success mb-3">
                         <i class="fas fa-plus"> Tambah Ketegori</i></button></a>
+            
+           <div class="dropdown d-inline">
+                <button class="btn btn-warning dropdown-toggle mb-3" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-expanded="false">
+                    Export File
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="<?= base_url('user/excel'); ?>">EXCEL</a>
+                    <!-- <a class="dropdown-item" href="#">PDF</a> -->
+                </div>
+            </div>
 
                 <!-- tabel kategori -->
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
