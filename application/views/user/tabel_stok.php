31b5c27f938f0735a03312974910d501257716ed
ArvendaAnggara <arvenda.119140089@student.itera.ac.id>
2022-05-17 20:36:38 +0700

    update fix

--- application/views/user/tabel_stok.php
+++ application/views/user/tabel_stok.php
@@ -49,7 +49,7 @@
 <div class="container-fluid">
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary"><?= $title2; ?></h6>
+            <h6 class="m-0 font-weight-bold text-dark"><?= $title2; ?></h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
