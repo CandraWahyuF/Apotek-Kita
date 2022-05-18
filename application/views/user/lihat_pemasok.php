ecde0e6620afcb3d5194c6a6a2caa86e7137158d
candrawahyuf <83354350+CandraWahyuF@users.noreply.github.com>
2022-05-18 13:06:25 +0700

    fitur download pd pemasok

--- application/views/user/lihat_pemasok.php
+++ application/views/user/lihat_pemasok.php
@@ -19,7 +19,7 @@
             <a href="<?php echo base_url('user/form_pemasok'); ?>"><button class="btn btn-success mb-3">
                     <i class="fas fa-plus"> Tambah Pemasok</i></button></a>
 
-            
+
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
@@ -59,3 +59,23 @@
     </div>
 </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#dataTable').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'excel', 'pdf',
+        ]
+    });
+});
+</script>
